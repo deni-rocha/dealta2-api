be6204bc736c2231fc6a6d9f8fe1bea48e324757
Denilson Rocha <denilsonrocha_@outlook.com>
2023-01-18 13:40:34 -0300

    new test

--- api/index.ts
+++ api/index.ts
@@ -1,19 +1,6 @@
-import express = require("express")
-import dotenv = require("dotenv")
-import { Router, Request, Response } from "express"
+import type { VercelRequest, VercelResponse } from "@vercel/node"
 
-dotenv.config()
-const app = express()
-const route = Router()
-
-route.get("/", (req: Request, res: Response) => {
+export default function (req: VercelRequest, res: VercelResponse) {
   const { name = "World" } = req.query
-  return res.status(200).json({
-    success: true,
-    message: "Sucesso!" + name
-  })
-})
-
-app.use(route)
-const port = process.env.PORT || 4000
-app.listen(port, () => console.log(`tá rodando na porta ${port}`))
+  res.send(`Hello ${name}!`)
+}
